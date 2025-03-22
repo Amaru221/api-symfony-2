78b3078bef415863fa3e1b07044e2172d930723b
null221 <agd1923@gmail.com>
2025-03-22 15:50:17 +0100

    added parameter currentUser default null, returned the user id or null

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,15 +3,16 @@
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class SecurityController extends AbstractController {
 
     #[Route('/login', name: 'app_login', methods: ["POST"])]
-    public function login() :Response {
+    public function login(#[CurrentUser] $user = null) :Response {
 
-
-
-        return $this->render('security/login.html.twig');
+        return $this->json([
+            'user' => $user ? $user->getId() : null,
+        ]);
     }
 
     
