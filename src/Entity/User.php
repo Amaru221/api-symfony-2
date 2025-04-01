ac8c11111ff314b7eaa7b87bf00325cb4ead04e0
null221 <agd1923@gmail.com>
2025-04-01 21:53:24 +0200

    created method getValidTokensString

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -219,4 +219,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getValidTokensString(): array{
+        return $this->getApiTokens()
+            ->filter(fn (ApiToken $token) => $token->isValid())
+            ->map(fn (ApiToken $token) => $token->getToken())
+            ->toArray()
+        ;
+    }
+
 }
