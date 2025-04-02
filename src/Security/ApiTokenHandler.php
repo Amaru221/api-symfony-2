3dbe589a4f3512bd113a9cde60e2fe01bdc12ae1
null221 <agd1923@gmail.com>
2025-04-02 11:44:51 +0200

    created apiTokenHandler. Created inherited function getUserBadgeFrom

---
+++ src/Security/ApiTokenHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Repository\ApiTokenRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+
+class ApiTokenHandler extends AccessTokenHandlerInterface
+{
+
+    public function __construct(private ApiTokenRepository $apiTokenRepository){
+    
+    }
+    
+    public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge{
+        //TODO
+        $token = $this->apiTokenRepository->findOneBy(['token' => $accessToken]);
+
+        if(!$token){
+            throw new BadCredentialsException();
+        }
+
+        return new UserBadge($token->getOwnedBy()->getUserIdentifier());
+    }
+
+}
