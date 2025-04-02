f199c182e28059ccfe02e39beb207ffe4ec8fcf7
null221 <agd1923@gmail.com>
2025-04-02 12:38:37 +0200

    added attribute decorator. Override invoke method and replaced securityScheme with new arguments type and scheme

--- src/ApiPlatform/OpenApiFactoryDecorator.php
+++ src/ApiPlatform/OpenApiFactoryDecorator.php
@@ -2,10 +2,11 @@
 namespace App\ApiPlatform;
 
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model\SecurityScheme;
 use ApiPlatform\OpenApi\OpenApi;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
-#[AsDecorator('')]
+#[AsDecorator('api_platform.openapi.factory')]
 class OpenApiFactoryDecorator implements OpenApiFactoryInterface {
 
     public function __construct(private OpenApiFactoryInterface $decorated){
@@ -15,6 +16,13 @@ class OpenApiFactoryDecorator implements OpenApiFactoryInterface {
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = $this->decorated->__invoke($context);
+
+        $securitySchemes = $openApi->getComponents()->getSecuritySchemes() ? : new \ArrayObject();
+
+        $securitySchemes['access_token'] = new SecurityScheme(
+            type: 'http',
+            scheme: 'bearer',
+        );
 
         return $openApi;
     }
