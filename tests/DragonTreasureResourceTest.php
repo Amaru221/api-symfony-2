be0697208a659f4cc3e37fa00fd98e384ba31d4d
null221 <agd1923@gmail.com>
2025-04-04 07:30:45 +0200

    added resetdatabase use statement

--- tests/DragonTreasureResourceTest.php
+++ tests/DragonTreasureResourceTest.php
@@ -2,10 +2,13 @@
 
 use Zenstruck\Browser\Test\HasBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class DragonTreasureResourceTest extends KernelTestCase {
 
+    use ResetDatabase;
     use HasBrowser;
+
 
     public function testGetCollectionOfTreasures(): void {
         $this->browser()
