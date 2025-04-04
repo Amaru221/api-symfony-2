17bdabd1a3c8238f12a24b6304c5fe424b795d50
null221 <agd1923@gmail.com>
2025-04-04 07:26:46 +0200

    created method test for getCollectionTreasure, dumping data

--- tests/DragonTreasureResourceTest.php
+++ tests/DragonTreasureResourceTest.php
@@ -1,7 +1,17 @@
 <?php
 
+use Zenstruck\Browser\Test\HasBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class DragonTreasureResourceTest extends KernelTestCase {
+
+    use HasBrowser;
+
+    public function testGetCollectionOfTreasures(): void {
+        $this->browser()
+        ->get('/api/treasures')
+        ->dump();
+    }
+
 
 }
