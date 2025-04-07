27747c56b7fbf7c868a93c6ec306db3a24468424
null221 <agd1923@gmail.com>
2025-04-07 13:18:12 +0200

    averrided method browser to add header accept jsonld

---
+++ tests/Functional/ApiTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+use Zenstruck\Browser\HttpOptions;
+use Zenstruck\Browser\Test\HasBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class ApiTestCase extends KernelTestCase {
+
+    use HasBrowser {
+        browser as baseKernelBrowser;
+    }
+
+    protected function browser(array $options = [], array $server = []) {
+        return $this->baseKernelBrowser($options, $server)
+            ->setDefaultHttpOptions(
+                HttpOptions::create()
+                    ->withHeader('Accept', 'application/ld+json')
+            );
+    }
+
+}
