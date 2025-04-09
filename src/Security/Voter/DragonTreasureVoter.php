c934535330e6d072525330e8ad96cd18b692f168
null221 <agd1923@gmail.com>
2025-04-09 12:40:56 +0200

    added assert to verify subject is a dragonTreasure

--- src/Security/Voter/DragonTreasureVoter.php
+++ src/Security/Voter/DragonTreasureVoter.php
@@ -2,6 +2,7 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\DragonTreasure;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -20,12 +21,13 @@ class DragonTreasureVoter extends Voter
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        return false;
         $user = $token->getUser();
         // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
             return false;
         }
+
+        assert($subject instanceof DragonTreasure);
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
