694907d7e2a1e2f7dd01e5324e197206a5fce263
null221 <agd1923@gmail.com>
2025-04-09 11:19:33 +0200

    added comments for description class and method

--- tests/Functional/ApiTestCase.php
+++ tests/Functional/ApiTestCase.php
@@ -6,12 +6,14 @@ use Zenstruck\Browser\HttpOptions;
 use Zenstruck\Browser\Test\HasBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+/** esta clase reemplaza el metodo browser de kernelTestCase para añadir nuestra propia opcion de cabecera */
 abstract class ApiTestCase extends KernelTestCase {
 
     use HasBrowser {
         browser as baseKernelBrowser;
     }
 
+    /** añadimos la cabecera accept, a application/ld+json para que solicite siempre este formato */
     protected function browser(array $options = [], array $server = []) {
         return $this->baseKernelBrowser($options, $server)
             ->setDefaultHttpOptions(
