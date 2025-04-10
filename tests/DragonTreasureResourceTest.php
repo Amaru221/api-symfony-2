fa92d1880de5ad08d394c139fee68932f79c6b3f
null221 <agd1923@gmail.com>
2025-04-10 18:38:11 +0200

    new test to try seePublished without admin user

--- tests/DragonTreasureResourceTest.php
+++ tests/DragonTreasureResourceTest.php
@@ -136,5 +136,25 @@ class DragonTreasureResourceTest extends ApiTestCase {
         ;
     }
 
+    public function testOwnerCanSeeIsPublishedField() : void {
+        $user = UserFactory::new()->create();
+        $treasure = DragonTreasureFactory::createOne([
+            'isPublished' => false,
+            'owner' => $user,
+        ]);
+
+        $this->browser()
+            ->actingAs($user)
+            ->patch('/api/treasures/'. $treasure->getId(), [
+                'json' => [
+                    'value' => 12345,
+                ],
+            ])
+            ->assertStatus(200)
+            ->assertJsonMatches('value', 12345)
+            ->assertJsonMatches('isPublished', false)
+        ;
+    }
+
 
 }
