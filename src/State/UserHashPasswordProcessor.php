8b6c7d8381e1979744a55db6d40d5ddd1335b5a6
null221 <agd1923@gmail.com>
2025-04-11 09:53:52 +0200

    added construct. setup decorator class with same arguments

--- src/State/UserHashPasswordProcessor.php
+++ src/State/UserHashPasswordProcessor.php
@@ -7,8 +7,17 @@ use ApiPlatform\State\ProcessorInterface;
 
 class UserHashPasswordProcessor implements ProcessorInterface
 {
+
+    public function __construct(private ProcessorInterface $innerProcessor)
+    {
+        
+    }
+
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
         // Handle the state
+        dump("is ALIVE!");
+
+        $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
