b2b2e1123a154faa0e6a12e2e55c5cf1ee29e2d0
null221 <agd1923@gmail.com>
2025-04-11 10:05:16 +0200

    verify if data is a user and have plainPassword. if yes, setPassword hashed in data

--- src/State/UserHashPasswordProcessor.php
+++ src/State/UserHashPasswordProcessor.php
@@ -2,15 +2,17 @@
 
 namespace App\State;
 
+use App\Entity\User;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class UserHashPasswordProcessor implements ProcessorInterface
 {
 
-    public function __construct(private ProcessorInterface $innerProcessor)
+    public function __construct(private ProcessorInterface $innerProcessor, private UserPasswordHasherInterface $userPasswordHasher)
     {
         
     }
@@ -18,7 +20,9 @@ class UserHashPasswordProcessor implements ProcessorInterface
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
         // Handle the state
-        dump("is ALIVE!");
+        if($data instanceof User && $data->getPlainPassword()){
+            $data->setPassword($this->userPasswordHasher->hashPassword($data, $data->getPlainPassword()));
+        }
 
         $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
