eb53db73d83c801318e36692223ae0199c362c59
null221 <agd1923@gmail.com>
2025-04-15 16:10:20 +0200

    config decorator class. Low-level normalizer decorated. Add dinamic group.

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -2,18 +2,39 @@
 
 namespace App\Normalizer;
 
+use App\Entity\DragonTreasure;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+#[AsDecorator('api_platform.jsonld.normalizer.item')]
+class AddOwnerGroupsNormalizer implements NormalizerInterface, SerializerAwareInterface {
 
-class AddOwnerGroupsNormalizer implements NormalizerInterface {
+
+    public function __construct(private NormalizerInterface $normalizer, private Security $security)
+    {
+        
+    }
+
 
     public function normalize(mixed $object, ?string $format = null, array $context = [])
     {
-        //TODO
+        if($object instanceof DragonTreasure && $this->security->getUser() === $object->getOwner()){
+            $context['groups'][] = 'owner:read';
+        }
+        return $this->normalizer->normalize($object, $format, $context);
     }
 
     public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
     {
-        //TODO
+        return $this->normalizer->supportsNormalization($data, $format);
+    }
+
+    public function setSerializer(SerializerInterface $serializer){
+        if($this->normalizer instanceof SerializerAwareInterface){
+            $this->normalizer->setSerializer($serializer);
+        }
     }
 
 }
