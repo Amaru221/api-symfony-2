940a238d07a94bc349fadc61e6860eec76b85b90
null221 <agd1923@gmail.com>
2025-04-15 10:20:55 +0200

    created new class adminGroupsContextBuilder, added extended method createFromRequest

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -1,3 +1,19 @@
 <?php
 
 namespace App\ApiPlatform;
+
+use Symfony\Component\HttpFoundation\Request;
+use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+
+
+
+class AdminGroupsContextBuilder extends SerializerContextBuilderInterface {
+
+    
+
+    public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array{
+        //TODO
+
+    }
+
+}
