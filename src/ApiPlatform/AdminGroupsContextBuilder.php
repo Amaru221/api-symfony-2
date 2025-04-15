39874ec3d43a6e0cbf529779f4b8d71d501a463b
null221 <agd1923@gmail.com>
2025-04-15 10:31:14 +0200

    added asDecorator attribute with serializer context builder id service

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -5,9 +5,9 @@ namespace App\ApiPlatform;
 use ApiPlatform\GraphQl\Serializer\SerializerContextBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
-
-
+#[AsDecorator('api_platform.serializer.context_builder')]
 class AdminGroupsContextBuilder extends SerializerContextBuilderInterface {
 
     public function __construct(private SerializerContextBuilderInterface $decorated)
@@ -18,6 +18,7 @@ class AdminGroupsContextBuilder extends SerializerContextBuilderInterface {
     public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array{
         //TODO
         $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
+        dump('I AM WORKING!');
 
         return $context;
     }
