a1a077d75122c854b4c33568b5a98c773978db5d
null221 <agd1923@gmail.com>
2025-04-15 10:26:46 +0200

    added construct to inyect dependency SerializerContextBuilder

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -2,6 +2,7 @@
 
 namespace App\ApiPlatform;
 
+use ApiPlatform\GraphQl\Serializer\SerializerContextBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use ApiPlatform\Serializer\SerializerContextBuilderInterface;
 
@@ -9,7 +10,10 @@ use ApiPlatform\Serializer\SerializerContextBuilderInterface;
 
 class AdminGroupsContextBuilder extends SerializerContextBuilderInterface {
 
-    
+    public function __construct(private SerializerContextBuilder $decorated)
+    {
+        
+    }
 
     public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array{
         //TODO
