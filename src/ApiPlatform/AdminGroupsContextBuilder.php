409c77fe7b05c2bdc6de648755387131a18940c2
null221 <agd1923@gmail.com>
2025-04-15 10:30:01 +0200

    decorate our service createFromRequest

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -10,14 +10,16 @@ use ApiPlatform\Serializer\SerializerContextBuilderInterface;
 
 class AdminGroupsContextBuilder extends SerializerContextBuilderInterface {
 
-    public function __construct(private SerializerContextBuilder $decorated)
+    public function __construct(private SerializerContextBuilderInterface $decorated)
     {
         
     }
 
     public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array{
         //TODO
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
 
+        return $context;
     }
 
 }
