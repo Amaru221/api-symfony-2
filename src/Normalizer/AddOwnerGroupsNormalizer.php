b3348d6b432670b7cee0fe2e859c36c74421f3fd
null221 <agd1923@gmail.com>
2025-04-15 15:49:18 +0200

    created new class to  normalizer groups for owners

---
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class AddOwnerGroupsNormalizer implements NormalizerInterface {
+
+    public function normalize(mixed $object, ?string $format = null, array $context = [])
+    {
+        //TODO
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null)
+    {
+        //TODO
+    }
+
+}
