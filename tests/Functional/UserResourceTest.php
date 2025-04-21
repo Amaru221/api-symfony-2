b540e213d94087de7c7daf28254d6024a25d3a1d
null221 <agd1923@gmail.com>
2025-04-21 19:20:40 +0200

    create test for valid not change owners

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\DragonTreasureFactory;
 use App\Factory\UserFactory;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -45,5 +46,27 @@ class UserResourceTest extends ApiTestCase {
         ;
 
     }
+
+    public function testTreasuresCannotBeStolen(): void
+    {
+        $user = UserFactory::createOne();
+        $otherUser = UserFactory::createOne();
+        $dragonTreasure = DragonTreasureFactory::createOne(['owner' => $otherUser]);
+
+        $this->browser()
+        ->actingAs($user)
+        ->patch('/api/users/'. $user->getId(), [
+            'json' => [
+                'username' => 'changed',
+                'dragonTreasures' => [
+                    '/api/treasures/'. $dragonTreasure->getId(),
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json']
+            ]
+        ])
+        ->assertStatus(422);
+
+    }
+
 
 }
