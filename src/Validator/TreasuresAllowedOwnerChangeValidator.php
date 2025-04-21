d9e8a889d360f9897c59dd3ac0fc400486fa398c
null221 <agd1923@gmail.com>
2025-04-21 19:34:29 +0200

    added construct with inyection em. Obtained unitOfWork to read the previous data

--- src/Validator/TreasuresAllowedOwnerChangeValidator.php
+++ src/Validator/TreasuresAllowedOwnerChangeValidator.php
@@ -2,18 +2,30 @@
 
 namespace App\Validator;
 
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class TreasuresAllowedOwnerChangeValidator extends ConstraintValidator
 {
+
+    public function __construct(private EntityManager $entityManager)
+    {
+
+    }
+
     public function validate($value, Constraint $constraint)
     {
-        /* @var App\Validator\TreasuresAllowedOwnerChange $constraint */
+        assert($constraint instanceof TreasuresAllowedOwnerChange);
 
         if (null === $value || '' === $value) {
             return;
         }
+
+        assert($value instanceof Collection);
+
+        $unitOfWork = $this->entityManager->getUnitOfWork();
 
         // TODO: implement the validation here
         $this->context->buildViolation($constraint->message)
