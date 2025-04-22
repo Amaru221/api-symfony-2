199499bb9da5a5bf9b60098f9165ddf7dd0aad44
null221 <agd1923@gmail.com>
2025-04-22 17:19:37 +0200

    implement queryExtension for unique item. Implement method applyToItem with the same logic than collection

--- src/ApiPlatform/DragonTreasureIsPublishedExtension.php
+++ src/ApiPlatform/DragonTreasureIsPublishedExtension.php
@@ -2,12 +2,13 @@
 namespace App\ApiPlatform;
 
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use Doctrine\ORM\QueryBuilder;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Entity\DragonTreasure;
 
-class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface{
+class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface{
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
@@ -20,6 +21,23 @@ class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInte
         $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
         ->setParameter('isPublished', true);
 
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, ?Operation $operation = null, array $context = []): void
+    {
+
+        $this->addIsPublishedWhere($queryBuilder, $resourceClass);
+
+    }
+
+    public function addIsPublishedWhere(QueryBuilder $queryBuilder, string $resourceClass) {
+        if(DragonTreasure::class !== $resourceClass){
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+        ->setParameter('isPublished', true);
     }
 
 }
