2c03bc2eab6e3984e9f59d8327598e9482a6da56
null221 <agd1923@gmail.com>
2025-04-22 16:09:15 +0200

    verify if dragonTreasure object sended have not owner, and a user is logged, then set this user to owner

--- src/State/DragonTreasureSetOwnerProcessor.php
+++ src/State/DragonTreasureSetOwnerProcessor.php
@@ -4,6 +4,7 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\DragonTreasure;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
@@ -18,6 +19,10 @@ class DragonTreasureSetOwnerProcessor implements ProcessorInterface
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
+
+        if($data instanceof DragonTreasure && $data->getOwner() === null && $this->security->getUser()){
+            $data->setOwner($this->security->getUser());
+        }
         // Handle the state
         $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
