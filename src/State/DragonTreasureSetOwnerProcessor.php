aa884c3dc7a3d9fe89268be478ad02f901a3dd38
null221 <agd1923@gmail.com>
2025-04-22 16:02:28 +0200

    added construct with ProcessorInterface inyection, called the inyection in process function with the params

--- src/State/DragonTreasureSetOwnerProcessor.php
+++ src/State/DragonTreasureSetOwnerProcessor.php
@@ -7,8 +7,15 @@ use ApiPlatform\State\ProcessorInterface;
 
 class DragonTreasureSetOwnerProcessor implements ProcessorInterface
 {
+
+    public function __construct(private ProcessorInterface $innerProcessor)
+    {
+        
+    }
+
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
         // Handle the state
+        $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
