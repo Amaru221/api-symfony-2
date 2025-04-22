7342aada7bf45981432ee67c0bf81b508ac22579
null221 <agd1923@gmail.com>
2025-04-22 16:32:12 +0200

    verify if the resourceClass is DragonTreasure to execute the queryExtension

--- src/ApiPlatform/DragonTreasureIsPublishedExtension.php
+++ src/ApiPlatform/DragonTreasureIsPublishedExtension.php
@@ -5,11 +5,17 @@ use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use Doctrine\ORM\QueryBuilder;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
+use App\Entity\DragonTreasure;
 
 class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface{
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
+        if(DragonTreasure::class !== $resourceClass){
+            return;
+        }
+
+
         
     }
 
