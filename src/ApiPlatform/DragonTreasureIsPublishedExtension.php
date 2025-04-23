b7f6757acf49ca23acd04125a3dfc1c96d7802ef
null221 <agd1923@gmail.com>
2025-04-23 16:30:59 +0200

    added security inyection

--- src/ApiPlatform/DragonTreasureIsPublishedExtension.php
+++ src/ApiPlatform/DragonTreasureIsPublishedExtension.php
@@ -1,14 +1,19 @@
 <?php
 namespace App\ApiPlatform;
 
-use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
-use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
-use Doctrine\ORM\QueryBuilder;
-use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
-use ApiPlatform\Metadata\Operation;
 use App\Entity\DragonTreasure;
+use Doctrine\ORM\QueryBuilder;
+use ApiPlatform\Metadata\Operation;
+use Symfony\Bundle\SecurityBundle\Security;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 
 class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface{
+
+    public function __construct(private Security $security){
+        
+    }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
